98e122cfee5414f36280a7070acf85ef2b9e8a69
Cecilia <lcecilia8147@gmail.com>
2024-09-22 18:46:00 -0300

    se empieza a mejorar codigo para poder iterar el json

--- ecommerce-product-page-main/script/script.js
+++ ecommerce-product-page-main/script/script.js
@@ -35,16 +35,22 @@ makeRequest("GET", "/data.json")
   .then(function (datums) {
    
     datums = JSON.parse(datums);
+    carrito = datums;
+
+    let titulo =   document.getElementById("titleProduct").innerText;
+    let description =  document.getElementById("descriptionProduct").innerText;
+    let price =  document.getElementById("precio").innerText;
 
     /**Cuando se carguen mas datos ver como iterar dinamicamente este objeto **/
-    document.getElementById("titleProduct").innerText = datums[0].title;
-   
+    for (let i = 0; i < carrito.length; i++) {
+      console.log(carrito[i].title)
+      titulo = carrito[i].title;
+      description = carrito[i].description;
+      price = "$"+carrito[i].price;
 
-    titulo = datums[0].title;
-    console.log(titulo);
-    document.getElementById("descriptionProduct").innerText =
-      datums[0].description;
+    }
 
+    
     /**Carga de imagenes **/
     crearDivImg(datums[0].picture[0], " active");
     for (let i = 0; i < datums[0].picture.length; i++) {
@@ -101,19 +107,26 @@ const btnAdd = document.getElementById("btnAdd");
  * cantidad
  * precio total
  */
+
 btnAdd.addEventListener("click",(e)=>{
   e.preventDefault();
   e.stopPropagation();
- 
-  let nombreProducto = document.getElementById('titleProduct').innerText;
-  let precio = document.getElementsByClassName("precioDescontado")[0].innerText;
+
+  let nombreProducto ;
+  for (let i = 0; i < carrito.length; i++) {
+    let nombreProducto = carrito[i].title;
+    console.log("titulo", nombreProducto)
+  }
+ circulo = document.createElement("div");
+ circulo.className = "bg-secondary";
+  
+ /* let precio = document.getElementsByClassName("precioDescontado")[0].innerText;
   let cantidad = document.getElementsByClassName("value")[0].innerText;
  let foto = document.getElementById("img-shoos");
  let basketIcon =document.getElementsByClassName("container-nav-avatar")[0];
 
 
- circulo = document.createElement("div");
- circulo.className = "bg-secondary";
+ 
  basketIcon.style.position = "relative";
 
   parseInt(cantidad);
@@ -121,9 +134,9 @@ btnAdd.addEventListener("click",(e)=>{
 
   let precioTotal = cantidad * precio;
 
-  carrito.push({"title": nombreProducto, "precio": precio, "cantidad": cantidad, "precioTotal": precioTotal, "foto": foto})
+  carrito.push({"id": 1,"title": nombreProducto, "precio": precio, "cantidad": cantidad, "precioTotal": precioTotal, "foto": foto})
   
-  //circulo.style.border = "solid orange";   
+     */
   circulo.style.borderRadius = "50%";   
   circulo.style.color ="white";
   circulo.style.width = "20px";
@@ -131,19 +144,39 @@ btnAdd.addEventListener("click",(e)=>{
   circulo.style.position = "absolute";
   circulo.style.top = "-7px";
   circulo.style.right = "57px";
-  circulo.innerText = cantidad;
+//  circulo.innerText = cantidad;
   circulo.style.textAlign= "center";
   circulo.style.fontSize= "12px";
   basketIcon.appendChild(circulo);
 
 })
 
+let actualProduct = () => {
+
+
+}
+let  btnDelete = document.getElementsByClassName("deleteButton")[0];
+
+let padre = document.getElementsByClassName("card")[0];
+let hijo = document.getElementsByClassName("card-body")[0];
+let hijo2 = document.getElementsByClassName("btnCheckout")[0];
+
+ //Elimina producto del carrito
+ let deleteProduct = () => {
+  if(padre){
+   
+      console.log("estoy en delete")
+      padre.removeChild(hijo);
+      padre.removeChild(hijo2);
+  }
+
+ }
+
+
 //añade la tarjeta carrito
 const addCard = () => {
   let box = document.getElementsByClassName("card")[0];
   if (!box) {
-
-
     let caja = document.createElement("div");
     caja.className = "card";
     caja.style.position = "absolute";
@@ -174,33 +207,48 @@ const addCard = () => {
     p.className = "card-title text-warning";
     p.style.fontSize = "15px"
     minimg = document.createElement("img");
-    trash = document.createElement("img");
+    let trash = document.createElement("img");
+    let trashLink = document.createElement("a");
+    let button = document.createElement("button");
+    button.className = "btn btn-secondary btnCheckout";
+    button.innerText = "Checkout";
 
-  
     if(carrito[0]){
       for(let i = 0; i < carrito.length; i++){
 
           minimg.src = carrito[i].foto.src
           p.innerText =  `${carrito[i].title} \n ${carrito[i].precio} x ${carrito[i].cantidad} =  ${carrito[i].precioTotal} `;    
           trash.src = "/ecommerce-product-page-main/images/icon-delete.svg"
+          console.log("productoID", carrito[i].id);
       }
      
     }else {
       p.innerText = "Your cart is empty";
       p.style.textAlign = "center";
     }
+
+
     minimg.style.width = "60px";
     minimg.style.borderRadius = "5px";
     minimg.style.height= "fit-content";
     minimg.style.marginRight = "16px";
+
     trash.style.width = "20px";
     trash.style.height = "fit-content";
     trash.style.marginLeft = "5px";
 
+    trashLink.className="deleteButton";
+    trashLink.appendChild(trash);
+    
+    button.style.width = "90%";
+    button.style.padding= "3% 0 3% 0";
+    button.style.margin = "15px";
+
+
     bodyCard.appendChild(minimg);
     bodyCard.appendChild(p);
-    bodyCard.appendChild(trash);
-    
+    bodyCard.appendChild(trashLink);
+    caja.appendChild(button);
  
 
     let parentDiv = slide.parentNode;
@@ -208,10 +256,17 @@ const addCard = () => {
     parentDiv.insertBefore(caja, slide);
   } else {
     box.remove();
-    console.log("entre al if");
+    
   }
 };
 
+/* btnDelete.addEventListener("click", (e)=>{
+
+  e.preventDefault();
+  e.stopPropagation();
+  deleteProduct();
+});
+ */
 let menos = document.getElementsByClassName("minus")[0];
 let mas = document.getElementById("plus");
 let valor = document.getElementsByClassName("value")[0];
